1113fe680c9c5f25f871b81e8db693668606a3be
Oric <oricricardo@gmail.com>
2022-09-05 15:01:08 -0300

    ad

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("oi")
+function Validar(){
+    var senha = document.getElementById("senha").value;
+    console.log(senha)
+    if (senha == "rrds"){
+        alert("Bem vindo")
+    }
+    else{
+        alert("Sai fora")
+    }
+}
