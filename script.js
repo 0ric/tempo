7da2cd5cad30c77bcae4c56f7e154bf76b3000d1
0ric <oricricardo@gmail.com>
2022-09-08 19:30:14 -0300

    começo site

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ function Validar(){
         alert("Bem vindo")
     }
     else{
-        alert("Sai fora")
+        alert("Erro")
     }
+}
+
+var lista = ['Nome 1','Nome 2','Nome 3'];
+function PreencherSelect(){
+    var htmlFinal = '';
+    var tamanhoLista = lista.length;
+    for (var i = 0; i < tamanhoLista; i++) {
+        htmlFinal += "<option>"+ lista [i] +"</option>"
+    }
+    document.getElementById('meuSelect').innerHTML = htmlFinal;
+}
+
+function AdicionarItem() {
+    var valorInput = document.getElementById('inputItem').value;
+    lista.push(valorInput);
+    PreencherSelect();
+}
+function apagarItem(){
+    var valorSelecionado = document.getElementById('meuSelect').value;
+    lista.splice(lista.indexOf(valorSelecionado),1);
+    PreencherSelect();  
 }
