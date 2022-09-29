97225fec6cba611048ea3c29053f2a3392f8e8e0
0ric <oricricardo@gmail.com>
2022-09-29 19:30:00 -0300

    terminando de estilizar o site

---
+++ clima.js
@@ -0,0 +1,8 @@
+const timeEL = document.getElementById('time');
+const dateEL = documents.getElementById('date');
+const currentWeatherItemsEL = documents.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryEL = document.getElementById('country');
+const weatherForecastEL = document.getElementById('weather-forecast');
+const currentTempEL = document.getElementById('current-temp');
+
