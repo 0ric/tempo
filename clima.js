ea356924aef4aa009932969fe55e4f5b52c7c238
0ric <oricricardo@gmail.com>
2022-10-06 19:28:48 -0300

    terminando de estiliazr

--- clima.js
+++ clima.js
@@ -1,8 +1,44 @@
-const timeEL = document.getElementById('time');
-const dateEL = documents.getElementById('date');
-const currentWeatherItemsEL = documents.getElementById('current-weather-items');
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
 const timezone = document.getElementById('time-zone');
-const countryEL = document.getElementById('country');
-const weatherForecastEL = document.getElementById('weather-forecast');
-const currentTempEL = document.getElementById('current-temp');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+
+const API_KEY = 'd4445de17eb2a36865f3f1b8e8102aa6';
+// datas e temporizador
+setInterval(() => {
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hour = time.getHours();
+  const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+  const minutes = time.getMinutes();
+  const ampm = hour >=12 ? 'PM' : 'AM'
+
+  timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+
+        hoursIn12HrFormat : hoursIn12HrFormat) + ':' +
+        (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">
+        ${ampm}</span>`
+  dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+
+}, 1000);
+getWeatherData();
+function getWeatherData(){
+    navigator.geolocation.getCurrentPosition((success)=>{
+      console.log(success);
+
+      let { latitude, longitude} = success.coords
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
+        .then(res => res.json())
+        .then(data =>{
+            consike.log(data);
+        } 
+    })
+}
 
